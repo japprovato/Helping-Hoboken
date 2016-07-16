3802e9ec36db9de9c7e9e367c5444a0213370e4c
Jordana Approvato <japprovato@JordanasMacBook.fios-router.home>
2016-07-16 14:22:01 -0400

    inital commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+/**
+ * This is the main application logic for our small angular app
+ * Here we define 'modules' -- angular's term to break applications into smaller pieces
+ *
+* */
+
+'use strict';
+// Wondering why this is a function wrapped in parenthesis?
+// It's a closure! They're great for hiding your code.
+// Without them variables are usually created globally, available to any other
+// scripts that could be running.
+(function() {})();
---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * A basic server for handling the Angular app and basic backend functions
+ */
+
+// Node lets us load in any json file as an object
+// __dirname is a node variable that holds the current working directory path
+var config = require(__dirname + '/config');
+var API_BASE = '/' + config.apiBase;
+
+var express = require('express');
+var app = express();
+
+// THIS SHOULD ALWAYS BE THE LAST CALL IN THIS FILE
+// The first parameter is the port, the second is an optional callback on success.
+app.listen(8080, function() {
+    console.log("App is running on localhost:8080");
+});
